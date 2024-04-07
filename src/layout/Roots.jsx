702567c85d1bb5825b9da311e2ab2e0b0ab85ee2
saiful797 <saifulislamsaif797@gmail.com>
2024-04-07 22:53:50 +0600

    Setup basic navbar and footer

--- src/layout/Roots.jsx
+++ src/layout/Roots.jsx
@@ -1,8 +1,12 @@
 import {Outlet} from 'react-router-dom'
+import NavBar from '../pages/Shared/NavBar/NavBar';
+import Footer from '../pages/footer/Footer';
 const Roots = () => {
     return (
-        <div className="font-poppins">
+        <div className="font-poppins max-w-7xl mx-auto">
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <div>
+            <h1 className="text-6xl font-bold">this is nav bar</h1>
+        </div>
+    );
+};
+
+export default NavBar;
