2a334865925aeffcf7a60dfa6bd04b8582d03d06
saiful797 <saifulislamsaif797@gmail.com>
2024-04-07 23:01:19 +0600

    Added demo footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,9 +1,34 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h1 className="text-6xl">This is footer</h1>
-        </div>
+        <footer className="p-10 bg-base-200 text-base-content text-center mt-10">
+            <div className="footer max-w-6xl grid  place-content-center md:place-content-between	">
+                <aside>
+                   <img className="w-20 rounded-3xl mx-auto" src="https://i.ibb.co/N9CjLr2/book-logo.pn" alt="Coming Soon...." />
+                    <p className="text-base"><span className="footer-title text-3xl">Demo Company</span><br/>Providing demo Since 1992</p>
+                </aside> 
+                <nav>
+                    <h6 className="footer-title">Services</h6> 
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav> 
+                <nav>
+                    <h6 className="footer-title">Company</h6> 
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav> 
+                <nav>
+                    <h6 className="footer-title">Legal</h6> 
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
+        </footer>
     );
 };
 
