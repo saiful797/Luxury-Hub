e9727f7f0044028c39773c08a65e908f8581d2b0
saiful797 <saifulislamsaif797@gmail.com>
2024-04-14 12:55:55 +0600

    Updated ViewPropertyDetais Page

--- src/viewPropertyDetails/ViewPropertyDetails.jsx
+++ src/viewPropertyDetails/ViewPropertyDetails.jsx
@@ -7,13 +7,20 @@ const ViewPropertyDetails = () => {
 
     const estate = estates.find(estate => estate.id === idNum);
 
-    const {estate_title} = estate;
+    const {image,estate_title,location,description} = estate;
 
     return (
         
-        <div className="mt-5">
-            <h1 className="text-center">Title: {estate_title}</h1>
-        </div>
+        <div className='md:flex bg-slate-200 rounded-2xl mt-5'>
+                <div className='md:w-[30%]'>
+                    <img className='w-full h-full rounded-2xl' src={image} alt="" />
+                </div>
+                <div className='md:w-[70%] text-left p-10 space-y-3'>
+                    <h1 className='text-2xl font-bold'>{estate_title}</h1>
+                    <p className='text-xl font-semibold'>Location: <span className='text-[#fdbb2d]'>{location}</span></p>
+                    <p><span className='text-xl font-medium text-[#60d845]'>Details: </span>: <span className='text-base font-medium'>{description}</span></p>
+                </div>
+            </div>
     );
 };
 
