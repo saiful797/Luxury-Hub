f986471ba919989c35d8d0667421c26b08bc66d5
saiful797 <saifulislamsaif797@gmail.com>
2024-04-14 13:24:36 +0600

    Updated View property details page

--- src/viewPropertyDetails/ViewPropertyDetails.jsx
+++ src/viewPropertyDetails/ViewPropertyDetails.jsx
@@ -7,18 +7,38 @@ const ViewPropertyDetails = () => {
 
     const estate = estates.find(estate => estate.id === idNum);
 
-    const {image,estate_title,location,description} = estate;
+    const {image,estate_title,segment_name,status,area,price,location,description,facilities} = estate;
 
     return (
         
-        <div className='md:flex bg-slate-200 rounded-2xl mt-5'>
-                <div className='md:w-[30%]'>
+        <div className='md:flex bg-blue-950 rounded-2xl mt-5 text-white'>
+                <div className='md:w-[40%]'>
                     <img className='w-full h-full rounded-2xl' src={image} alt="" />
                 </div>
-                <div className='md:w-[70%] text-left p-10 space-y-3'>
+                <div className='md:w-[60%] text-left p-10 space-y-3'>
                     <h1 className='text-2xl font-bold'>{estate_title}</h1>
+
+                    <h3>Property: <span className='font-bold'>{segment_name}</span></h3>
+
+                    <h5>
+                        <span className='text-lg font-medium'>Status: <span className='text-[#22c1c3]'>For</span> <span className='text-[#fdbb2d] hover:text-[#22c1c3] text-2xl '>{status}</span></span>
+                    </h5>
+
+                    <h4 className="text-xl font-thin">Area <span className="text-xl font-bold text-amber-400 hover:text-[#22c1c3]">: {area}</span></h4>
+
+                    <p className="text-xl font-medium"><span className='text-xl font-medium text-[#60d845]'>Price </span>: {price}</p>
+
                     <p className='text-xl font-semibold'>Location: <span className='text-[#fdbb2d]'>{location}</span></p>
-                    <p><span className='text-xl font-medium text-[#60d845]'>Details: </span>: <span className='text-base font-medium'>{description}</span></p>
+
+                    <p><span className='text-xl font-medium text-[#60d845]'>Details: </span> <span className='text-base font-medium'>{description}</span></p>
+
+                    <div className='flex gap-2'>
+                        <h1>Facilities: </h1>
+                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[0]}</p>
+                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[1]}</p>
+                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[2]}</p>
+                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[3]}</p>
+                    </div>
                 </div>
             </div>
     );
