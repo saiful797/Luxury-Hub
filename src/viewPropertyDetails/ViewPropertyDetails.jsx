0560ab2c82541cfe3f571ea832614d0c3e60c2bd
saiful797 <saifulislamsaif797@gmail.com>
2024-04-14 13:51:08 +0600

    Updated estate.json file and estate details page

--- src/viewPropertyDetails/ViewPropertyDetails.jsx
+++ src/viewPropertyDetails/ViewPropertyDetails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const ViewPropertyDetails = () => {
     const estates = useLoaderData();
@@ -12,35 +12,44 @@ const ViewPropertyDetails = () => {
     return (
         
         <div className='md:flex bg-blue-950 rounded-2xl mt-5 text-white'>
-                <div className='md:w-[40%]'>
-                    <img className='w-full h-full rounded-2xl' src={image} alt="" />
-                </div>
-                <div className='md:w-[60%] text-left p-10 space-y-3'>
-                    <h1 className='text-2xl font-bold'>{estate_title}</h1>
+            <div className='md:w-[40%]'>
+                <img className='w-full h-full rounded-2xl' src={image} alt="" />
+            </div>
+            <div className='md:w-[60%] text-left p-10 space-y-3'>
+                <h1 className='text-2xl font-bold'>{estate_title}</h1>
 
-                    <h3>Property: <span className='font-bold'>{segment_name}</span></h3>
+                <h3>Property: <span className='font-bold'>{segment_name}</span></h3>
 
-                    <h5>
-                        <span className='text-lg font-medium'>Status: <span className='text-[#22c1c3]'>For</span> <span className='text-[#fdbb2d] hover:text-[#22c1c3] text-2xl '>{status}</span></span>
-                    </h5>
+                <h5>
+                    <span className='text-lg font-medium'>Status: <span className='text-[#22c1c3]'>For</span> <span className='text-[#fdbb2d] hover:text-[#22c1c3] text-2xl '>{status}</span></span>
+                </h5>
 
-                    <h4 className="text-xl font-thin">Area <span className="text-xl font-bold text-amber-400 hover:text-[#22c1c3]">: {area}</span></h4>
+                <h4 className="text-2xl font-thin">Area: <span className="text-xl font-bold text-amber-400 hover:text-[#22c1c3]"> {area}</span></h4>
 
-                    <p className="text-xl font-medium"><span className='text-xl font-medium text-[#60d845]'>Price </span>: {price}</p>
+                <p className="text-xl font-medium"><span className='text-xl font-medium text-[#60d845]'>Price </span>: {price}</p>
 
-                    <p className='text-xl font-semibold'>Location: <span className='text-[#fdbb2d]'>{location}</span></p>
+                <p className='text-xl font-semibold'>Location: <span className='text-teal-500'>{location}</span></p>
 
-                    <p><span className='text-xl font-medium text-[#60d845]'>Details: </span> <span className='text-base font-medium'>{description}</span></p>
+                <p><span className='text-xl font-medium text-[#60d845]'>Details: </span> <span className='text-base font-medium'>{description}</span></p>
 
-                    <div className='flex gap-2'>
-                        <h1>Facilities: </h1>
-                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[0]}</p>
-                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[1]}</p>
-                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[2]}</p>
-                        <p className='text-green-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[3]}</p>
+                <div className='flex gap-2'>
+                    <h1 className="text-xl font-extrabold text-rose-500">Facilities: </h1>
+                    <div className="grid grid-cols-2">
+                        <p className='text-teal-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[0]}</p>
+                        <p className='text-teal-500 text-lg font-bold ml-10'><span className='text-[#fdbb2d]'>#</span>{facilities[1]}</p>
+                        <p className='text-teal-500 text-lg font-bold'><span className='text-[#fdbb2d]'>#</span>{facilities[2]}</p>
+                        <p className='text-teal-500 text-lg font-bold ml-10'><span className='text-[#fdbb2d]'>#</span>{facilities[3]}</p>
                     </div>
                 </div>
+
+                <Link to="/">
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-outline btn-success text-xl font-medium">Back to Home</button>
+                    </div>
+                </Link>
+                
             </div>
+        </div>
     );
 };
 
