a99691c610a4b79158e60eed65d4da57064f9727
saiful797 <saifulislamsaif797@gmail.com>
2024-04-15 13:11:22 +0600

    Setup context for use perpose

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
+import FirebaseProvider from './pages/FirebaseProvider/FirebaseProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+
+    <FirebaseProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </FirebaseProvider>
+    
   </React.StrictMode>,
 )
