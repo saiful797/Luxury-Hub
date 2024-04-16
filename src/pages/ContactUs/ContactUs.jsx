6dafb99f6e716ddab17ba3106069769442968517
saiful797 <saifulislamsaif797@gmail.com>
2024-04-16 22:27:46 +0600

    Added demo contact page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,8 @@
 
 const ContactUs = () => {
     return (
-        <div className="mt-5">
-            <p className="text-center text-teal-600 text-4xl font-medium">This is contact page</p>
+        <div className="mt-5 flex justify-center items-center">
+            <p className="text-center mx-auto text-teal-600 text-4xl font-medium"><img src="https://i.ibb.co/6yYzCYh/contact.png" alt="" /></p>
         </div>
     );
 };
