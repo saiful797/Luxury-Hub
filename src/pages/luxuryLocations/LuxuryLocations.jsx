8262f576b6c48a05da0688041e9dae6bb9430abe
saiful797 <saifulislamsaif797@gmail.com>
2024-04-16 21:52:12 +0600

    Updated world wide location page

--- src/pages/luxuryLocations/LuxuryLocations.jsx
+++ src/pages/luxuryLocations/LuxuryLocations.jsx
@@ -4,50 +4,50 @@ const LuxuryLocations = () => {
             <h1 className="text-2xl font-bold text-center">Luxury properties in top locations worldwide</h1>
             <hr />
             <hr />
-            <div className="flex mt-5">
-                <div className="">
-                    <p>With over 500,000 luxury properties in over 120 countries, LuxuryEstate.com provides the largest selection of luxury properties. More than 50,000 real estate agencies display their finest luxury properties on the website, the largest number of high-end agents on a single website. Our goal is to provide the easiest and most efficient way to search for exceptional</p>
-                    <div className="flex justify-around mt-5">
-                        <div>
-                            <h1 className="text-xl text-lime-600"><u>EUROPE</u></h1>
-                            <p>FRANCE</p>
-                            <p>ITALY</p>
-                            <p>SPAIN</p>
-                            <p>UNITED KINGDOM</p>
-                            <p>SWITZERLAND</p>
-                        </div>
-
-                        <div>
-                            <h1 className="text-xl text-lime-600"><u>NORTH AMERICA</u></h1>
-                            <p>BRITISH VIRGIN ISLANDS</p>
-                            <p>UNITED STATES</p>
-                            <p>MEXICO</p>
-                            <p>CANADA</p>
-                            <p>PANAMA</p>
-                        </div>
-                    </div>
+            <div className=" mt-5 gap-10">
+                <div className="flex gap-5">
+                    <p className="text-justify">With over 500,000 luxury properties in over 120 countries, LuxuryHub.com provides the largest selection of luxury properties. More than 50,000 real estate agencies display their finest luxury properties on the website, the largest number of high-end agents on a single website. Our goal is to provide the easiest and most efficient way to search for exceptional</p>
+                    <p className="text-justify">properties around the world, relying on trusted and independent professionals in luxury real estate. LuxuryHub.com will open your eyes to the most enchanting properties in the world: luxury homes, prestigious estates, classic residences, unique castles and country homes. To rediscover that beauty is not a luxury. It is a necessity.</p>
                 </div>
-                <div>
-                    <p>properties around the world, relying on trusted and independent professionals in luxury real estate. LuxuryHub.com will open your eyes to the most enchanting properties in the world: luxury homes, prestigious estates, classic residences, unique castles and country homes. To rediscover that beauty is not a luxury. It is a necessity.</p>
-                    <div className="flex justify-around mt-5">
-                        <div>
-                            <h1 className="text-xl text-lime-600"><u>SOUTH AMERICA</u></h1>
-                            <p>BRAZIL</p>
-                            <p>ARGENTINA</p>
-                            <p>CHILE</p>
-                            <p>URUGUAY</p>
-                            <p>PERU</p>
-                        </div>
 
-                        <div>
-                            <h1 className="text-xl text-lime-600"><u>ASIA</u></h1>
-                            <p>CHINA</p>
-                            <p>THAILAND</p>
-                            <p>UNITED ARAB EMIRATES</p>
-                            <p>TURKEY</p>
-                            <p>JAPAN</p>
-                        </div>
+                <div className="flex justify-between mt-5">
+                    
+                    <div>
+                        <h1 className="text-xl text-lime-600"><u>EUROPE</u></h1>
+                        <p>FRANCE</p>
+                        <p>ITALY</p>
+                        <p>SPAIN</p>
+                        <p>UNITED KINGDOM</p>
+                        <p>SWITZERLAND</p>
                     </div>
+
+                    <div>
+                        <h1 className="text-xl text-lime-600"><u>NORTH AMERICA</u></h1>
+                        <p>BRITISH VIRGIN ISLANDS</p>
+                        <p>UNITED STATES</p>
+                        <p>MEXICO</p>
+                        <p>CANADA</p>
+                        <p>PANAMA</p>
+                    </div>
+                
+                    <div>
+                        <h1 className="text-xl text-lime-600"><u>SOUTH AMERICA</u></h1>
+                        <p>BRAZIL</p>
+                        <p>ARGENTINA</p>
+                        <p>CHILE</p>
+                        <p>URUGUAY</p>
+                        <p>PERU</p>
+                    </div>
+
+                    <div>
+                        <h1 className="text-xl text-lime-600"><u>ASIA</u></h1>
+                        <p>CHINA</p>
+                        <p>THAILAND</p>
+                        <p>UNITED ARAB EMIRATES</p>
+                        <p>TURKEY</p>
+                        <p>JAPAN</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
